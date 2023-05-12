708a415293e6ca13bc7ec66bb96823e321f93284
Masih Jahangiri <masihjahan@gmail.com>
2023-05-13 00:48:09 +0330

    feat: improve config

--- config/elasticsearch_backup.sh
+++ config/elasticsearch_backup.sh
@@ -5,9 +5,9 @@ OUTPUT_FILE=/backups/${FILE}
 
 mkdir -p /backups
 ssh -oStrictHostKeyChecking=no -o ExitOnForwardFailure=yes -f -L 9200:localhost:${ES_PORT} ${SSH_USERNAME}@${SSH_HOST} -p ${SSH_PORT} sleep 10
-elasticdump --input=http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/  --output=/backups/data.json --type=data --size=5000
+elasticdump --input=http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/  --output=/backups/data.json --type=data --limit=5000
 ssh -oStrictHostKeyChecking=no -o ExitOnForwardFailure=yes -f -L 9200:localhost:${ES_PORT} ${SSH_USERNAME}@${SSH_HOST} -p ${SSH_PORT} sleep 10
-elasticdump --input=http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/  --output=/backups/mapping.json --type=mapping --size=5000
+elasticdump --input=http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/  --output=/backups/mapping.json --type=mapping --limit=5000
 
 tar -czvf $OUTPUT_FILE /backups/data.json /backups/mapping.json
 rm /backups/data.json /backups/mapping.json
