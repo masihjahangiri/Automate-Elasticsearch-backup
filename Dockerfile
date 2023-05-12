e3ea47d6504063c83c263fbafebddc6bf2c8ddd5
Masih Jahangiri <masihjahan@gmail.com>
2023-05-12 22:42:24 +0330

    init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:20-alpine3.16
+RUN apk update && apk upgrade
+RUN npm install elasticdump -g
+
+
+# copy crontabs for root user
+COPY config/cronjobs /etc/crontabs/root
+
+COPY config/id_rsa /root/.ssh/
+RUN chmod 600 ~/.ssh/id_rsa
+
+COPY config/elasticsearch_backup.sh /root/
+RUN chmod u+r+x /root/elasticsearch_backup.sh
+
+
+
+# start crond with log level 8 in foreground, output to stderr
+CMD ["crond", "-f", "-d", "8"]
---
+++ config/elasticsearch_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+FILE=`date +"%Y-%m-%d-%H_%M"`${FILE_SUFFIX}.tar.gz
+OUTPUT_FILE=/backups/${FILE}
+
+mkdir -p /backups
+ssh -oStrictHostKeyChecking=no -o ExitOnForwardFailure=yes -f -L 9200:localhost:${ES_PORT} ${SSH_USERNAME}@${SSH_HOST} -p ${SSH_PORT} sleep 10
+
+elasticdump --input=http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/  --output=/backups/data.json --type=data
+elasticdump --input=http://${ES_USERNAME}:${ES_PASSWORD}@localhost:9200/  --output=/backups/mapping.json --type=mapping
+
+tar -czvf $OUTPUT_FILE /backups/data.json /backups/mapping.json
+rm /backups/data.json /backups/mapping.json
+
+echo "${OUTPUT_FILE} was created:"
+ls -l ${OUTPUT_FILE}
+
+find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${FILE_SUFFIX}.tar.gz" -exec rm -rf '{}' ';'
